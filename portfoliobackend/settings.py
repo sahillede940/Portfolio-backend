2831afae8d84cb908870c719272d6e649f524c7e
Sahil Keshao Lede <98254859+sahillede940@users.noreply.github.com>
2023-09-10 04:21:05 +0530

    fixed buildd

--- portfoliobackend/settings.py
+++ portfoliobackend/settings.py
@@ -131,14 +131,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
 MEDIA_URL = 'media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = [BASE_DIR/'static',]
-STATIC_ROOT = BASE_DIR/'staticfiles'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -151,5 +150,6 @@ REST_FRAMEWORK={
 
 ALLOWED_HOSTS = [
     ".vercel.app",
-    '.now.sh'
+    '.now.sh',
+    '127.0.0.1'
 ]
