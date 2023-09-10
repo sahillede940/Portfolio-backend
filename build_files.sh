becf98d026899b633f2b6f8103e5450d81c4ac9b
Sahil Keshao Lede <98254859+sahillede940@users.noreply.github.com>
2023-09-10 18:15:55 +0530

    done vercel settings

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,4 @@
-pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
+echo "BUILDING FILES"
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
+echo "BUILD COMPLETE"
--- portfoliobackend/settings.py
+++ portfoliobackend/settings.py
@@ -137,7 +137,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
